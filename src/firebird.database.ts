46ca2544b05c9414ae1fb47b72e0802ee774c999
Vladimir Lastovka <elcopro@gmail.com>
2023-10-13 14:44:48 +0300

    pool

--- src/firebird.database.ts
+++ src/firebird.database.ts
@@ -1,8 +1,16 @@
-import { Database, Options, attach, create, attachOrCreate, Isolation } from 'node-firebird';
+import { Database, Options, attach, create, attachOrCreate, Isolation, pool, ConnectionPool } from 'node-firebird';
 import { promisify } from 'util';
 import FirebirdTransaction from './firebird.transaction';
 export default class FirebirdDatabase {
     private db: Database;
+    private pool: ConnectionPool;
+    private async getDb(): Promise<Database> {
+        if (this.db) {
+            return this.db;
+        }
+        const get = promisify(this.pool.get);
+        return get.call(this.pool);
+    }
     async attach(options: Options): Promise<FirebirdDatabase> {
         this.db = await promisify<Options, Database>(attach)(options);
         return this;
@@ -19,21 +27,29 @@ export default class FirebirdDatabase {
         const fd: FirebirdDatabase = new FirebirdDatabase();
         return fd.attach(options);
     }
+    static buildAndPool(sockets: number, options: Options): FirebirdDatabase {
+        const fd: FirebirdDatabase = new FirebirdDatabase();
+        fd.pool = pool(sockets, options);
+        return fd;
+    }
     private checkDb(): void {
-        if (!this.db) throw new Error('Create or Attach Database first');
+        if (!this.db && !this.pool) throw new Error('Create or Attach Database or Pool first');
     }
     async query(query: string, params: any[]): Promise<any[]> {
         this.checkDb();
-        const asyncQuery = promisify(this.db.query);
-        return asyncQuery.call(this.db, query, params);
+        const db = await this.getDb();
+        const asyncQuery = promisify(db.query);
+        return asyncQuery.call(db, query, params);
     }
     async execute(query: string, params: any[]): Promise<any[]> {
         this.checkDb();
-        return promisify(this.db.execute)(query, params);
+        const db = await this.getDb();
+        return promisify(db.execute)(query, params);
     }
     async transaction(isolation: Isolation): Promise<FirebirdTransaction> {
         this.checkDb();
-        const asyncTransaction = promisify(this.db.transaction);
-        return new FirebirdTransaction(await asyncTransaction.call(this.db, isolation));
+        const db = await this.getDb();
+        const asyncTransaction = promisify(db.transaction);
+        return new FirebirdTransaction(await asyncTransaction.call(db, isolation));
     }
 }
