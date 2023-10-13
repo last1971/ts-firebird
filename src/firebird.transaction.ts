2a47349f2211237d95c7b6fa714fd515db671d55
Vladimir Lastovka <elcopro@gmail.com>
2023-10-13 19:55:45 +0300

    find error

--- src/firebird.transaction.ts
+++ src/firebird.transaction.ts
@@ -1,8 +1,8 @@
-import { Database, Transaction } from 'node-firebird';
+import { Transaction } from 'node-firebird';
 import { promisify } from 'util';
 
 export default class FirebirdTransaction {
-    constructor(private transaction: Transaction, private db: Database = null) {}
+    constructor(private transaction: Transaction) {}
     async query(query: string, params: any[], autoCommit = false): Promise<any[]> {
         const asyncQuery = promisify(this.transaction.query);
         try {
@@ -28,11 +28,9 @@ export default class FirebirdTransaction {
     async commit(): Promise<void> {
         const asyncCommit = promisify(this.transaction.commit);
         await asyncCommit.call(this.transaction);
-        if (this.db) this.db.detach();
     }
     async rollback(): Promise<void> {
         const asyncRollback = promisify(this.transaction.rollback);
         await asyncRollback.call(this.transaction);
-        if (this.db) this.db.detach();
     }
 }
